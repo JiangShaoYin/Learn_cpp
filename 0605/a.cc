43f6f613566e01cee06f7b68fa153f95545d45ef
JiangShaoYin <tooo_cold@163.com>
2019-12-22 15:17:39 +0800

    add

--- 0605/a.cc
+++ 0605/a.cc
@@ -3,13 +3,15 @@
  /// @author  jiang(tooo_cold@163.com)
  /// @date    2018-06-05 23:53:27
  
+ 
 #include "inline.h"
 #include <iostream>
 using std::cout;
 using std::endl;
 
 int main(){
-	int a=3,b=4;
+
+	int a=3, b=4;//
 	cout<<add(a,b)<<endl;
 	return 0;
 }
